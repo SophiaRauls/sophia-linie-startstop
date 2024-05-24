b8010070d8a993793d75ecfa088b3693e7539b5b
SophiaRauls <sophiarauls@gmail.com>
2024-05-24 22:06:37 +0200

    logik

--- main.ts
+++ main.ts
@@ -12,11 +12,11 @@ basic.forever(function () {
     if (ichbinan == 1) {
         L = maqueen.readPatrol(maqueen.Patrol.PatrolLeft)
         R = maqueen.readPatrol(maqueen.Patrol.PatrolRight)
-        if (R != 0 && 0 != L) {
+        if (R == 0 && 0 == L) {
             basic.setLedColors(0x000000, 0xffffff, 0x000000)
-        } else if (R != 0 && 0 == L) {
-            basic.setLedColors(0x000000, 0x000000, 0xff00ff)
         } else if (R == 0 && 0 != L) {
+            basic.setLedColors(0x000000, 0x000000, 0xff00ff)
+        } else if (R != 0 && 0 == L) {
             basic.setLedColors(0x00ff00, 0x000000, 0x000000)
         } else {
             basic.setLedColors(0x00ffff, 0x00ffff, 0x00ffff)
