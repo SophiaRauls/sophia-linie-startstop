e014a48db7731799721c08e7164790c998a539a0
SophiaRauls <sophiarauls@gmail.com>
2024-05-24 21:46:45 +0200

    Sensoren abfragen

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 let ichbinan = 0
+let L = 0
+let R = 0
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     if (ichbinan == 1) {
         ichbinan = 0
@@ -8,7 +10,8 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
 })
 basic.forever(function () {
     if (ichbinan == 1) {
-    	
+        L = maqueen.readPatrol(maqueen.Patrol.PatrolLeft)
+        R = maqueen.readPatrol(maqueen.Patrol.PatrolRight)
     } else {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
     }
