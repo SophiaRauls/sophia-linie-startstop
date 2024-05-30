11c5a049b965ab289033af81f07fde1f6f82e4e8
SophiaRauls <sophiarauls@gmail.com>
2024-05-30 21:34:52 +0200

    Sophia hat die Lösung!

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 let ichbinan = 0
 let L = 0
 let R = 0
+let Merk = 0
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     if (ichbinan == 1) {
         ichbinan = 0
@@ -13,21 +14,30 @@ basic.forever(function () {
         L = maqueen.readPatrol(maqueen.Patrol.PatrolLeft)
         R = maqueen.readPatrol(maqueen.Patrol.PatrolRight)
         if (R == 0 && 0 == L) {
+            Merk = 0
             basic.setLedColors(0x000000, 0xffffff, 0x000000)
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 48)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 49)
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 49)
         } else if (R == 0 && 0 != L) {
+            Merk = 1
             basic.setLedColors(0x000000, 0x000000, 0xff00ff)
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 21)
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 21)
         } else if (R != 0 && 0 == L) {
+            Merk = -1
             basic.setLedColors(0x00ff00, 0x000000, 0x000000)
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 23)
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 23)
         } else {
-            basic.setLedColors(0x00ffff, 0x00ffff, 0x00ffff)
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+            if (Merk < 0) {
+                basic.setLedColors(0x00ff00, 0x00ff00, 0x00ff00)
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 23)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 23)
+            } else {
+                basic.setLedColors(0xff00ff, 0xff00ff, 0xff00ff)
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 21)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 21)
+            }
         }
     } else {
         basic.setLedColors(0x000000, 0x000000, 0x000000)
